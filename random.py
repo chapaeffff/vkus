2e78bec9b124a62f7806b6e2efeb486b3b2d68da
Eva2301 <34630400+Eva2301@users.noreply.github.com>
2018-02-17 16:13:33 +0400

    Update random.py

--- random.py
+++ random.py
@@ -1,8 +1,5 @@
 
 >>> user1=[i for i in range(1,15)]
 >>> user2 = [i for i in range(10,25)]
->>> user3=list(user1)
->>> user4=list(user2)
->>> set(user3).intersection(user4)
-{10, 11, 12, 13, 14}
+>>> common_rates = set(user3).intersection(user4)
 
