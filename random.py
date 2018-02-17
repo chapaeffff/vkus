495d8b0570f97d4c8821308af008b381e317c67a
Eva2301 <34630400+Eva2301@users.noreply.github.com>
2018-02-17 15:39:43 +0400

    Не очень поняла твою задачу, поэтому на такое)))

--- random.py
+++ random.py
@@ -1,8 +1,8 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+
 >>> user1=[i for i in range(1,15)]
 >>> user2 = [i for i in range(10,25)]
->>> set(user1).intersection(user2)
+>>> user3=list(user1)
+>>> user4=list(user2)
+>>> set(user3).intersection(user4)
 {10, 11, 12, 13, 14}
->>> 
-р
+
